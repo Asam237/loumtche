a775bf7d2d402879e9d7b32bd29effe39b836cb5
Asam <abbasalimokolo@gmail.com>
2021-07-22 08:57:48 +0100

    initialisation

---
+++ src/navigations/auth.navigation.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+export class AuthNavigation extends Component {
+  render() {
+    <Stack.Navigator>
+      <Stack.Screen />
+    </Stack.Navigator>;
+  }
+}
