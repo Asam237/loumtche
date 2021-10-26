0cae39bd0c557132120bbfbfef1a9ccc4c190f34
Asam237 <abbasalimokolo@gmail.com>
2021-10-26 08:58:32 +0100

    screen: Signup(ajout des textes dans la constante)

--- src/screens/signup/Signup.screen.tsx
+++ src/screens/signup/Signup.screen.tsx
@@ -1,8 +1,21 @@
 import React from "react"
-import { Text } from "react-native"
+import { SafeAreaView, StyleSheet } from "react-native"
+import { COLOR } from "../../assets/themes/global.themes"
+import { Layout } from "../layout/Layout.screen"
 
 export const Signup = () => {
     return (
-        <Text>Signup</Text>
+        <SafeAreaView style={styles.container}>
+            <Layout>
+
+            </Layout>
+        </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: COLOR.primaryColor
+    }
+})
